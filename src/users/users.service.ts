bd95dddec8f3b73c4022f54c997bdeb4aa8ad3df
SoyTiyi <santiago.marmartinez@gmail.com>
2026-01-15 16:04:38 -0500

    add createFromWebhook

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -21,21 +21,48 @@ export class UsersService {
 
   async completeOnboarding(clerkId: string, data: OnboardingDto) {
     const user = await this.prisma.user.update({
-        where: { clerkId },
-        data: {
-            companyName: data.companyName,
-            category: data.category,
-            address: data.address,
-            onboardingComplete: true,
-        },
+      where: { clerkId },
+      data: {
+        companyName: data.companyName,
+        category: data.category,
+        address: data.address,
+        onboardingComplete: true,
+      },
     });
 
     await clerkClient.users.updateUser(clerkId, {
-        publicMetadata: {
-            onboardingComplete: true,
-        },
+      publicMetadata: {
+        onboardingComplete: true,
+      },
     });
 
     return user;
+  }
+
+  async createFromWebhook(data: {
+    clerkId: string;
+    email: string;
+    firstName?: string;
+    lastName?: string;
+    imageUrl?: string;
+  }) {
+    return this.prisma.user.upsert({
+      update: {
+        email: data.email,
+        firstName: data.firstName,
+        lastName: data.lastName,
+        imageUrl: data.imageUrl,
+      },
+
+      create: {
+        clerkId: data.clerkId,
+        email: data.email,
+        firstName: data.firstName,
+        lastName: data.lastName,
+        imageUrl: data.imageUrl,
+        onboardingComplete: false,
+      },
+      where: { clerkId: data.clerkId },
+    });
   }
 }
