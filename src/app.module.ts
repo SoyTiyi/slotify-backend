eaea5b5ba94d0ee2435e4be084196333f95078cb
SoyTiyi <santiago.marmartinez@gmail.com>
2026-01-15 16:44:50 -0500

    add webhooks module and controller file

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
+import { WebhooksModule } from './webhooks/webhooks.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule],
+  imports: [PrismaModule, UsersModule, WebhooksModule],
   controllers: [AppController],
   providers: [AppService],
 })
