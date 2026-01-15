0d3b3b49efa512e37fa97072d83dfe0058810970
SoyTiyi <santiago.marmartinez@gmail.com>
2026-01-15 15:57:22 -0500

    create clerk-client and use it con completeOnboarding method in user.service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { OnboardingDto } from './dto/onboarding.dto';
+import { clerkClient } from 'src/clerk/clerk-client';
 
 @Injectable()
 export class UsersService {
@@ -13,6 +15,26 @@ export class UsersService {
     if (!user) {
       throw new NotFoundException('User not found');
     }
+
+    return user;
+  }
+
+  async completeOnboarding(clerkId: string, data: OnboardingDto) {
+    const user = await this.prisma.user.update({
+        where: { clerkId },
+        data: {
+            companyName: data.companyName,
+            category: data.category,
+            address: data.address,
+            onboardingComplete: true,
+        },
+    });
+
+    await clerkClient.users.updateUser(clerkId, {
+        publicMetadata: {
+            onboardingComplete: true,
+        },
+    });
 
     return user;
   }
